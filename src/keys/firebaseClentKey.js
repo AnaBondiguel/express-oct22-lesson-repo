051edc95853ae11a543af392a8c7aa10f63c53a5
AnaBondiguel <michaelyangyang@hotmail.com>
2022-10-17 16:13:16 +1100

    setup firebase

---
+++ src/keys/firebaseClentKey.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCh148qbNPe3Veg4Ixp_LP_Eo78txC78pQ",
+    authDomain: "expressjsoct22.firebaseapp.com",
+    projectId: "expressjsoct22",
+    storageBucket: "expressjsoct22.appspot.com",
+    messagingSenderId: "461980858371",
+    appId: "1:461980858371:web:f57a8caeeda4415589f636",
+    measurementId: "G-CXF0G9R9TP"
+  };
+
+  module.exports = { firebaseConfig}
